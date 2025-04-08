6da63edb06bb4a1bc198e3499dc8b87903bf0581
Cassiebastress <cassie.bastress@gmail.com>
2025-04-08 15:25:40 -0400

    Finished writing circ_align for Issue 30

--- circ_align.py
+++ circ_align.py
@@ -1,7 +1,9 @@
 from issue3 import align_seqs
+from Bio.Seq import Seq
+from typing import List
 
 
-def permutateSeq(seq, places):
+def permutateSeq(seq: Seq, places: int) -> Seq:
     """
     Permutate a sequence by a given number of places
     """
@@ -9,10 +11,10 @@ def permutateSeq(seq, places):
     newSeq = []
     for i in range(len(seq)):
         newSeq.append(seq[(i + places) % len(seq)])
-    return "".join(newSeq)
+    return Seq("".join(newSeq))
 
 
-def score_alignment(seq1, seq2):
+def score_alignment(seq1: str, seq2: str) -> float:
     """
     Score an alignment
     """
@@ -31,3 +33,23 @@ def score_alignment(seq1, seq2):
     score = matches / length
 
     return round(score, 2)
+
+
+def circular_align(seq1: Seq, seq2: Seq) -> List[Seq]:
+    """
+    Return the best alignment of two sequences
+    """
+    if (len(seq1) != len(seq2)):
+        raise ValueError("Sequences must be the same length")
+    highest_score = 0
+    best_alignment = [seq1, seq2]  # default to the original sequences
+    for i in range(len(seq1)):
+        cur_permutation = permutateSeq(seq1, i)
+        aligned_seqs = align_seqs([cur_permutation, seq2])
+        score = score_alignment(aligned_seqs[0], aligned_seqs[1])
+        if score == 1.0:
+            return aligned_seqs
+        elif score > highest_score:
+            highest_score = score
+            best_alignment = aligned_seqs
+    return best_alignment
